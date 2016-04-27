788e1c27d99b461c2226e74fb20373b0977f489e
Karoru19 <mystika19@wp.pl>
2016-04-27 14:34:32 +0200

    Adding new classes

--- Programowanie/Projekt/mecz.cpp
+++ Programowanie/Projekt/mecz.cpp
@@ -1,6 +1,15 @@
 #include "mecz.h"
 
-Mecz::Mecz()
-{
+Mecz::Mecz(SedziaGlowny Sedzia, int PunktyGospodarza, int PunktyGoscia) : m_PunktyGospodarza(PunktyGospodarza), m_PunktyGoscia(PunktyGoscia) , m_Sedzia(Sedzia) {
 
+}
+
+int Mecz::PunktyGospodarza() const
+{
+    return m_PunktyGospodarza;
+}
+
+int Mecz::PunktyGoscia() const
+{
+    return m_PunktyGoscia;
 }
--- Programowanie/Projekt/mecz.h
+++ Programowanie/Projekt/mecz.h
@@ -1,12 +1,18 @@
 #ifndef MECZ_H
 #define MECZ_H
 
-
+#include "sedziaglowny.h"
 
 class Mecz
 {
+private:
+    int m_PunktyGospodarza;
+    int m_PunktyGoscia;
+    SedziaGlowny m_Sedzia;
 public:
-    Mecz();
+    Mecz (SedziaGlowny Sedzia, int PunktyGospodarza = 0, int PunktyGoscia = 0);
+    int PunktyGospodarza() const;
+    int PunktyGoscia() const;
 };
 
 #endif // MECZ_H
